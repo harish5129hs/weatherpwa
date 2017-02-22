771a7d0f4f056845decf39cf9b2a8f846e64dc26
harish <harish.saini@gwynniebee.com>
2017-02-22 14:57:26 +0530

    first commit in this repo

---
+++ service-worker.js
@@ -0,0 +1,78 @@
+var cacheName = 'weatherPWA-step-6-v5';
+var dataCacheName='weatherData-v7';
+
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+   e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key!= dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(e) {
+  //console.log('[ServiceWorker] Fetch', e.request.url);
+  
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+      	console.log("[ServiceWorker] fethching data from server ->",e.request.url);
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+  		e.respondWith(
+		    caches.match(e.request).then(function(response) {
+		      if(response){
+		      	console.log("[ServiceWorker] found in cache ->",e.request.url);
+		      	return response;
+		      }
+		      console.log("[ServiceWorker] fethching from server ->",e.request.url);
+		      return fetch(e.request);
+		    })
+		);
+   } 
+  
+});
